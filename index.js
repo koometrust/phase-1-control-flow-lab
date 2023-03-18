6493ced302311da2c0182aedd724cdf8137f7c4b
Koome <judekoome360@gmail.com>
2023-03-18 13:14:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let response;
+  if (feet <= 400) {
+    response = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2500) { // Fixed the condition to be less than or equal to 2500
+    response = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    response = 'No can do.';
+  }
+  return response;
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC") ? "Ok, sounds good." : "No go."; // Used ternary operator to check if city is "NYC"
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
